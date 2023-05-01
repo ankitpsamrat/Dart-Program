46ee42b9f8c0268d5f9864ee3a318cfd4e4b01f1
ankitpsamrat <ankit.p.samrat@gmail.com>
2023-05-01 22:00:43 +0530

    change

--- keyword_final.dart
+++ keyword_final.dart
@@ -1,9 +1,7 @@
 void main(List<String> args) {
   final name = 'Ankit';
 
-  //  its give error because final value can't assign another value
-
-  // name = 'India';
+  // name = 'India';   //  its give error because final value can't assign another value
 
   print(name);
 }
