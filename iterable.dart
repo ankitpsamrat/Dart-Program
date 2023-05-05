d47fe14ea43bd40c8661692a878733fde2f6076c
ankitpsamrat <ankit.p.samrat@gmail.com>
2023-05-05 19:27:45 +0530

    new file added

---
+++ iterable.dart
@@ -0,0 +1,9 @@
+void main(List<String> args) {
+  //  In Dart, an Iterable is a collection of objects that can be accessed sequentially. It provides methods for accessing, iterating, and manipulating the collection of objects. It is an abstract class that is implemented by many concrete classes like List, Set, and Queue.
+
+  //  In short, a List is a type of Iterable, but not all Iterables are Lists. Lists have additional methods and properties specific to their implementation, such as the ability to access elements by index, whereas other Iterables like Sets and Queues do not necessarily support index-based access.
+
+  const iterable = ['Salad', 'Popcorn', 'Toast'];
+
+  print(iterable);
+}
