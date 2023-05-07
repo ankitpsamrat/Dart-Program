4fbc62a51e00ec0c214afa3259dd2bc2062b3d1f
ankitpsamrat <ankit.p.samrat@gmail.com>
2023-05-07 22:37:43 +0530

    changes

--- variable.dart
+++ variable.dart
@@ -1,7 +1,9 @@
 void main(List<String> args) {
   var name = 'SAMRAT'; //  here name is a varible
   int age = 20; //  here age is a varible
+  Object name2 = 'Bob';
 
   print(name);
   print(age);
+  print(name2);
 }
