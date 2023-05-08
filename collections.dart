13e7ae12ce0463fca620f082ddcd87086d60dba9
ankitpsamrat <ankit.p.samrat@gmail.com>
2023-05-08 23:05:13 +0530

    new file added

---
+++ collections.dart
@@ -0,0 +1,21 @@
+//  collection of list
+List list = [
+  'Car',
+  'Boat',
+  'Plane',
+];
+
+//  collection of set
+Set halogens = {
+  'fluorine',
+  'chlorine',
+  'bromine',
+};
+
+//  collection of map
+Map gifts = {
+  // Key:    Value
+  'first': 'partridge',
+  'second': 'turtledoves',
+  'fifth': 'golden rings',
+};
