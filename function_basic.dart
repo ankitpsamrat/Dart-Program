991ec74ec78c79fbbc6a3238292c929ef37fd2cc
ankitpsamrat <ankit.p.samrat@gmail.com>
2023-05-09 23:47:56 +0530

    new file added

---
+++ function_basic.dart
@@ -0,0 +1,14 @@
+//  function syntext
+
+//  returnType  functionName(functionParameters){
+//  code to be executed
+//  return value;
+// }
+
+//   functions which contain more expression
+int num(int a, int b) {
+  return a + b;
+}
+
+//   functions which contain only one expression
+int num2(int a) => a;
