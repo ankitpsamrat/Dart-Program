63336d42e64655737c38bc625b9ef5baa4643600
ankitpsamrat <ankit.p.samrat@gmail.com>
2023-05-09 23:48:40 +0530

    new file added

---
+++ function_parameters.dart
@@ -0,0 +1,21 @@
+//  named parameters
+
+//  method 1
+
+// void printName(String firstName, String lastName, {String? middleName}) {
+//   print('$firstName ${middleName ?? ''} $lastName');
+// }
+
+// void main(List<String> args) {
+//   printName('Ankit', 'Samrat', middleName: 'Pratap');
+// }
+
+//  method 2
+
+void printName(String firstName, String lastName, {String middleName = ''}) {
+  print('$firstName $middleName $lastName');
+}
+
+void main(List<String> args) {
+  printName('Ankit', 'Samrat', middleName: 'Pratap');
+}
