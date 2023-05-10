7912f2d8410460586d318534024d58eb2d3b3430
ankitpsamrat <ankit.p.samrat@gmail.com>
2023-05-10 23:43:36 +0530

    new file added

---
+++ function_type.dart
@@ -0,0 +1,32 @@
+void main(List<String> args) {
+  //  function with no argument & no return value
+
+  // myfun() {
+  //   print('Hello function');
+  // }
+
+  // myfun();
+
+  //  function with no argument but with return value
+
+  // int addfun() {
+  //   int a = 10;
+  //   int b = 20;
+  //   return a + b;
+  // }
+
+  // var total = addfun();
+  // print(total);
+
+  //  function with argument but no return value
+
+  int addfun(a, b) {
+    return a + b;
+  }
+
+  var total = addfun(10, 10);
+  print(total);
+
+  //  function with argument & return value
+  
+}
