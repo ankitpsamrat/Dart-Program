d364a18a31930c301fcda05103e14c29389dc464
ankitpsamrat <ankit.p.samrat@gmail.com>
2023-05-18 22:31:48 +0530

    new file added

---
+++ constructor_parameterized.dart
@@ -0,0 +1,12 @@
+// ignore_for_file: unused_local_variable
+
+class PC {
+  final String name;
+  final int age;
+
+  PC({this.name = 'Ankit', this.age = 21});
+}
+
+void main(List<String> args) {
+  PC value = new PC();
+}
