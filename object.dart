883ce2f9e3f099ad3ab373b60fb6ea86aae7a29b
ankitpsamrat <ankit.p.samrat@gmail.com>
2023-06-21 19:10:16 +0530

    modifie

--- object.dart
+++ object.dart
@@ -1,14 +1,15 @@
-class Student { 
-   void test_method() { 
-      print("This is a  test method"); 
-   } 
-   
-   void test_method1() { 
-      print("This is a  test method1"); 
-   } 
-}  
+class Student {
+  void test_method() {
+    print("This is a  test method");
+  }
+
+  void test_method1() {
+    print("This is a  test method1");
+  }
+}
+
 void main(List<String> args) {
-   Student s1 = new Student(); 
-   s1.test_method(); 
-   s1.test_method1(); 
+  Student s1 = new Student();
+  s1.test_method();
+  s1.test_method1();
 }
--- operator_relational.dart
+++ operator_relational.dart
@@ -3,7 +3,7 @@ void main(List<String> args) {
   var b = 200;
 
   print(a == b); //  equal to
-  print(a != b); //  not equal to 
+  print(a != b); //  not equal to
   print(a > b); //  greater than
   print(a < b); //  less than
   print(a >= b); //  greater than or equal to
