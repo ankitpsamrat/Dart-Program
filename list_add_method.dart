59360083ce8f41acfaa593672cc4496b3fa425e5
ankitpsamrat <ankit.p.samrat@gmail.com>
2023-08-12 23:41:57 +0530

    changes

--- list_add_method.dart
+++ list_add_method.dart
@@ -1,10 +1,28 @@
 void main(List<String> args) {
-  var boy = [];
-
   //  add value in empty list
+  // var boy = [];
 
-  boy.add('Vishal');
-  boy.add('Samrat');
+  // boy.add('Vishal');
+  // boy.add('Samrat');
+  // print(boy);
 
-  print(boy);
+  //  Combining two lists
+  List s = ['Samrat', 'Rohit', 'Raja'];
+  List s1 = ['Welcome'];
+
+  //  mehtod 1
+  // s1.addAll(s);
+  // print(s1);
+
+  //  method 2
+  // List data = s1 + s;
+  // print(data);
+
+  //  method 3
+  // List data1 = [...s1, ...s];
+  // print(data1);
+
+  //  method 4
+  List data2 = new List.from(s1)..addAll(s);
+  print(data2);
 }
