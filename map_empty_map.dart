f5175885d05c1a4bf2d499da8262c6ab0b41d17d
ankitpsamrat <ankit.p.samrat@gmail.com>
2023-08-14 22:28:48 +0530

    changes

--- map_empty_map.dart
+++ map_empty_map.dart
@@ -8,11 +8,9 @@ void main(List<String> args) {
   // print(address1);
 
   //  add value into empty map
-
   Map address3 = {};
   address3['name'] = 'Samrat';
   address3['state'] = 'U.P.';
   address3['country'] = 'India';
-
   print(address3);
 }
