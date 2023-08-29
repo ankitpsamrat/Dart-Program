2e620f4f8ccb59cc25ebbbb2bfb088857f3b04dd
ankitpsamrat <ankit.p.samrat@gmail.com>
2023-08-29 23:58:28 +0530

    changes

--- class2.dart
+++ class2.dart
@@ -1,10 +1,23 @@
-class TestClass {
-  void disp() {
-    print("Hello World");
-  }
+// class TestClass {
+//   void disp() {
+//     print("Hello World");
+//   }
+// }
+
+// void main(List<String> args) {
+//   TestClass c = new TestClass();
+//   c.disp();
+// }
+
+class User {
+  final String name;
+  final int age;
+
+  User(this.name, this.age);
 }
 
 void main(List<String> args) {
-  TestClass c = new TestClass();
-  c.disp();
+  final data = User('Ankit', 21);
+  print(data.name);
+  print(data.age);
 }
