901d1fa579df7a8e1e882faad4412b03047b4a7f
ankitpsamrat <ankit.p.samrat@gmail.com>
2023-09-22 23:49:51 +0530

    added

---
+++ set.dart
@@ -0,0 +1,14 @@
+// Dart program to show the Sets concept
+void main(List<String> args) {
+// Declaring set in First Way
+  var s1 = <String>{'Ankit'};
+
+// Printing First Set
+  print("Output of first set: $s1");
+
+// Declaring set in Second Way
+  Set<String> s2 = {'Ankit'};
+
+// Printing Second Set
+  print("Output of second set: $s2");
+}
