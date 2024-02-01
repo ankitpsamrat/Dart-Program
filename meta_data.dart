7f3db762a94172836d37a8d7065e1d04abb37bd0
ankitpsamrat <ankit.p.samrat@gmail.com>
2024-02-01 23:40:06 +0530

    added

---
+++ meta_data.dart
@@ -0,0 +1,12 @@
+class Television {
+  /// Use [turnOn] to turn the power on instead.
+  @Deprecated('Use turnOn instead')
+  void activate() {
+    turnOn();
+  }
+
+  /// Turns the TV's power on.
+  void turnOn() {
+    // ···
+  }
+}
