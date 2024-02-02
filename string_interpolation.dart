a7824fe9c83bd1c761a3664130c50f5dfb68c308
ankitpsamrat <ankit.p.samrat@gmail.com>
2024-02-02 22:22:58 +0530

    added

---
+++ string_interpolation.dart
@@ -0,0 +1,12 @@
+void main(List<String> args) {
+  // use $ followed by a variable name
+  // or expression within curly braces {}
+
+  // method 1
+  String name = 'Ankit';
+  print('My name is: $name');
+
+  // method 2
+  double age = 21.8;
+  print('My age: ${age.floor()}');
+}
