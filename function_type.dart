594c5271994b67e68130075b4ea43518cccf187b
ankitpsamrat <ankit.p.samrat@gmail.com>
2024-02-08 22:43:17 +0530

    changes

--- function_type.dart
+++ function_type.dart
@@ -1,31 +1,30 @@
 void main(List<String> args) {
-  //  function with no argument & no return value
-
-  // myfun() {
-  //   print('Hello function');
-  // }
-
-  // myfun();
-
-  //  function with no argument but with return value
-
-  // int addfun() {
-  //   int a = 10;
-  //   int b = 20;
-  //   return a + b;
-  // }
-
-  // var total = addfun();
-  // print(total);
-
-  //  function with argument but no return value
-
-  int addfun(a, b) {
-    return a + b;
+  // function with no argument & no return value
+  void displayGreeting() {
+    print('Hello, world!');
   }
 
-  var total = addfun(10, 10);
-  print(total);
+  displayGreeting();
 
-  //  function with argument & return value
+  // function with no argument but with return value
+  int getCurrentHour() {
+    return DateTime.now().hour;
+  }
+
+  int currentHour = getCurrentHour();
+  print('The current hour is: $currentHour');
+
+  // function with argument but no return value
+  void getUser(String name) {
+    print('Hello, $name!');
+  }
+
+  getUser('Ankit');
+
+  // function with argument & return value
+  bool isEven(int number) {
+    return number % 2 == 0;
+  }
+
+  print(isEven(3));
 }
