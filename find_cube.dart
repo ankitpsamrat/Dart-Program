6f2e918cc4618858625fea05e6e70922fc21d654
ankitpsamrat <ankit.p.samrat@gmail.com>
2024-02-11 22:29:12 +0530

    added

---
+++ find_cube.dart
@@ -0,0 +1,12 @@
+double cube(double num) {
+  return num * num * num;
+}
+
+void main(List<String> args) {
+  // simple method
+  double a = 3;
+  print(a * a * a);
+
+  // using function
+  print(cube(5));
+}
