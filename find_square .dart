b434396adae3e4abafc59b808dc413fb599645de
ankitpsamrat <ankit.p.samrat@gmail.com>
2024-02-11 22:29:04 +0530

    added

---
+++ find_square .dart
@@ -0,0 +1,12 @@
+double square(double num) {
+  return num * num;
+}
+
+void main(List<String> args) {
+  // simple method
+  double a = 2;
+  print(a * a);
+
+  // using function
+  print(square(5));
+}
