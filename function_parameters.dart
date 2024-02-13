b7e2c7fbd78f56549060fb268bed892303390b16
ankitpsamrat <ankit.p.samrat@gmail.com>
2024-02-13 22:46:39 +0530

    changes

--- function_parameters.dart
+++ function_parameters.dart
@@ -1,23 +1,23 @@
-//  named parameters
-//  When defining a function, use {param1, param2, …}
-//  to specify named parameters
+// when defining a function, use {param1, param2, …}
+// to specify named parameters
 
-//  method 1
+// when parameter has optional value
+void exa1({String? name, int? age}) {
+  print('Name: $name, Age: $age');
+}
 
-// void printName(String firstName, String lastName, {String? middleName}) {
-//   print('$firstName ${middleName ?? ''} $lastName');
-// }
+// when parameter has default value
+void exa2({String name = 'Ankit', int age = 21}) {
+  print('Name: $name, Age: $age');
+}
 
-// void main(List<String> args) {
-//   printName('Ankit', 'Samrat', middleName: 'Pratap');
-// }
-
-//  method 2
-
-void printName(String firstName, String lastName, {String middleName = ''}) {
-  print('$firstName $middleName $lastName');
+// when parameter has required value
+void exa3({required String name, required int age}) {
+  print('Name: $name, Age: $age');
 }
 
 void main(List<String> args) {
-  printName('Ankit', 'Samrat', middleName: 'Pratap');
+  exa1(name: 'Ankit', age: 21);
+  exa2(name: 'Samrat', age: 22);
+  exa3(name: 'Pratap', age: 21);
 }
