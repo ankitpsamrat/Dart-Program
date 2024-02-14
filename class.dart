8454e7d64eab26d87234b624674b174ac16caab3
ankitpsamrat <ankit.p.samrat@gmail.com>
2024-02-14 23:06:58 +0530

    changes

--- class.dart
+++ class.dart
@@ -1,12 +1,28 @@
-class Car {
-  String engine = "E1001";
+// syntaxt
 
-  void disp() {
-    print(engine);
+// <class keyword> ClassName{
+//   variables, methods(functions) to be executed
+// }
+
+class Person {
+  // variables
+  String name = 'Ankit';
+  int age = 21;
+
+  // functions
+  String greet() => 'Hello, my name is $name!';
+
+  bool isAdult() {
+    return true;
   }
 }
 
-void main(List<String> args) {
-  Car c = new Car();
-  c.disp();
+void main() {
+  // create object to access data
+  Person obj = Person();
+
+  print(obj.name);
+  print(obj.age);
+  print(obj.greet());
+  print(obj.isAdult());
 }
