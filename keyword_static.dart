1901d45cbab2032611929eb01c6192500037ca90
ankitpsamrat <ankit.p.samrat@gmail.com>
2024-02-18 22:54:52 +0530

    changes

--- keyword_static.dart
+++ keyword_static.dart
@@ -1,11 +1,17 @@
-class StaticMem {
-  static int? num;
-  static disp() {
-    print("The value of num is ${StaticMem.num}");
+// if a data member is static, it can be
+// accessed without creating an object.
+
+class User {
+  static String name = 'Ankit Samrat';
+  static int rollNo = 223534534;
+
+  static int mobileNo() {
+    return rollNo;
   }
 }
 
 void main(List<String> args) {
-  StaticMem.num = 12;
-  StaticMem.disp();
+  print(User.name);
+  print(User.rollNo);
+  print(User.mobileNo());
 }
