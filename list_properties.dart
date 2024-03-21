59f86a9dd58e443f379f71ead53f3a678df373e6
ankitpsamrat <ankit.p.samrat@gmail.com>
2024-03-21 22:57:06 +0530

    changes

--- list_properties.dart
+++ list_properties.dart
@@ -1,27 +1,31 @@
 void main(List<String> args) {
   List s = ['Samrat', 'Rohit', 'Raja'];
 
-  //  for checking the length of list
+  // to check the length of list
   // print(s.length);
 
-  //  for reverse the value of list
+  // to reverse the value of list
   // print(s.reversed);
 
-  //  for find the first value from list
+  // to find the first value from list
   // print(s.first);
 
-  //  for find the last value from list
+  // to find the last value from list
   // print(s.last);
 
-  //  for remove any value from list
+  // to remove any value from list
   // s.remove('Rohit');
   // print(s);
 
-  //  for remove any value from specific location from lis
+  // to remove any value using index of list
   // s.removeAt(0);
   // print(s);
 
-  //  for remove last value from list
-  s.removeLast();
+  // to remove last value from list
+  // s.removeLast();
+  // print(s);
+
+  // to remove all value from list
+  s.clear();
   print(s);
 }
