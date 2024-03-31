46f20184d90a4d6d39d413776e4c138fc8dc321d
ankitpsamrat <ankit.p.samrat@gmail.com>
2024-03-31 22:36:57 +0530

    changes

--- stream.dart
+++ stream.dart
@@ -1,22 +1,14 @@
-import 'dart:async';
+// streams are a sequence of asynchronous events.
+
+Stream<int> countDown() async* {
+  for (var i = 10; i > 0; i--) {
+    yield i;
+    await Future.delayed(Duration(seconds: 1));
+  }
+}
 
 void main() {
-  // create a stream controller
-  final StreamController<int> _controller = StreamController<int>();
-
-  // create a stream
-  final Stream<int> _stream = _controller.stream;
-
-  // define a listener for the stream
-  _stream.listen((int data) {
-    print('Data: $data');
+  countDown().listen((event) {
+    print(event);
   });
-
-  // add data to the stream
-  _controller.sink.add(1);
-  _controller.sink.add(2);
-  _controller.sink.add(3);
-
-  // close the stream when done
-  _controller.close();
 }
