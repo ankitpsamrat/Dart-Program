32a28ffb94ec526c1ec7dd128e1d7be59c3bc6cc
ankitpsamrat <ankit.p.samrat@gmail.com>
2025-01-17 23:45:56 +0530

    pattern square

---
+++ pattern_exa.dart
@@ -0,0 +1,10 @@
+// print square pattern
+
+void main(List<String> args) {
+  for (int i = 0; i < 10; i++) {
+    for (int j = 0; j < 10; j++) {
+      print('*');
+    }
+    print('\n');
+  }
+}
--- test.dart
+++
@@ -1,6 +0,0 @@
-void main(List<String> args) {
-  
-  for (var i = 0; i < 10; i++) {
-    print('Ankit');
-  }
-}
