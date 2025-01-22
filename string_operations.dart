b8b154d0fb15b64095d1018e0785d136107b460f
ankitpsamrat <ankit.p.samrat@gmail.com>
2025-01-22 23:05:54 +0530

    added

---
+++ string_operations.dart
@@ -0,0 +1,11 @@
+void main(List<String> args) {
+  // string interpolation
+  String name = 'Ankit';
+  int age = 22;
+  print('My name is: $name and my age is: $age');
+
+  // concatenation
+  String firstName = 'Ankit';
+  String lastName = 'Samrat';
+  print('My name is: ' + firstName + ' ' + lastName);
+}
