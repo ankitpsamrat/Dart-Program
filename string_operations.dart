59e5e2ef1ffc9a75fdeb2efdc716bcfc3c11c343
ankitpsamrat <ankit.p.samrat@gmail.com>
2025-01-27 23:14:28 +0530

    changes

--- string_operations.dart
+++ string_operations.dart
@@ -8,4 +8,14 @@ void main(List<String> args) {
   String firstName = 'Ankit';
   String lastName = 'Samrat';
   print('My name is: ' + firstName + ' ' + lastName);
+
+  // append a word to a string
+  String s = 'Hello';
+  s += ' World';
+  print(s);
+
+  // append a character to a string
+  String s1 = 'Hello';
+  s1 += '!';
+  print(s1);
 }
