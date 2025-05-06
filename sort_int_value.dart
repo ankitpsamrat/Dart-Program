1a4bfcc72ed3a8124e8797673c947f77d482cacb
ankitpsamrat <ankit.p.samrat@gmail.com>
2025-05-06 23:14:39 +0530

    logic changed

--- sort_int_value.dart
+++ sort_int_value.dart
@@ -1,8 +1,13 @@
+List<int> sortIntValues(List<int> items) {
+  // sort in ascending order
+  // List<int> ascendingOrder = items..sort();
+  // return ascendingOrder;
+
+  // sort in decending order
+  List<int> decendingOrder = items..sort((a, b) => b.compareTo(a));
+  return decendingOrder;
+}
+
 void main(List<String> args) {
-  List<String> values = ["1", "1000", "10", "100", "200"];
-
-  //  sorting and storing the list
-  final sorted = values.map(int.parse).toList()..sort();
-
-  print(sorted);
+  print(sortIntValues([5, 2, 8, 1, 9, 3, 7, 4, 6]));
 }
